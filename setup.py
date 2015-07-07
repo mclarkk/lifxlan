a35fa41a45888ff108182278b00ce5d720d22fa1
mclarkk <meghan.leah.clark@gmail.com>
2015-07-07 03:47:55 -0400

    added bitstring dependency

--- setup.py
+++ setup.py
@@ -2,10 +2,13 @@ from setuptools import setup
 
 setup(name='lifxlan',
       version='0.1',
-      description='Local communication with LIFX bulbs over a LAN.',
+      description='API for local communication with LIFX devices over a LAN.',
       url='http://github.com/mclarkk/lifxlan',
       author='Meghan Clark',
       author_email='mclarkk@umich.edu',
-      license='GPL v2.0',
+      license='MIT',
       packages=['lifxlan'],
+      install_requires=[
+        "bitstring",
+	  ],
       zip_safe=False)
