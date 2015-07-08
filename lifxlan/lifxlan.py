1fd0abea6700991c75f72a7c3498e01559fc7d20
mclarkk <meghan.leah.clark@gmail.com>
2015-07-08 06:11:46 -0400

    added broadcast commands for get/set power and get/set color

--- lifxlan/lifxlan.py
+++ lifxlan/lifxlan.py
@@ -1,6 +1,6 @@
 from socket import socket, AF_INET, SOCK_DGRAM, SOL_SOCKET, SO_REUSEADDR, SO_BROADCAST, timeout
 from message import BROADCAST_MAC, BROADCAST_SOURCE_ID
-from device import Device, UDP_BROADCAST_IP, UDP_BROADCAST_PORT
+from device import Device, UDP_BROADCAST_IP, UDP_BROADCAST_PORT, DEFAULT_TIMEOUT, DEFAULT_ATTEMPTS
 from light import *
 from msgtypes import *
 from unpack import unpack_lifx_message
@@ -11,8 +11,12 @@ class LifxLAN:
 	def __init__(self, num_lights=None, verbose=False):
 		self.source_id = randint(0, (2**32)-1)
 		self.num_devices = num_lights
-		self.devices = []
+		self.num_lights = num_lights
+		self.devices = None
+		self.lights = None
 		self.verbose = verbose
+
+		self.lights = self.get_lights()
 
 	############################################################################
 	#                                                                          #
@@ -20,35 +24,86 @@ class LifxLAN:
 	#                                                                          #
 	############################################################################
 
-	def get_devices(self):
-		if self.num_devices == None:
-			responses = self.discover()
-		else:
-			responses = self.broadcast_with_resp(GetService, StateService)
-		for r in responses:
-			mac = r.target_addr
-			service = r.service
-			port = r.port
-			self.devices.append(Device(mac, service, port, self.source_id, self.verbose))
-		self.num_devices = len(self.devices)
-		return self.devices
+	# This is shuttered until it becomes clear how to distinguish between Lights and non-Light Devices
+	# def get_devices(self):
+	#	if self.num_devices == None:
+	#		responses = self.discover()
+	#	else:
+	#		responses = self.broadcast_with_resp(GetService, StateService)
+	#	for r in responses:
+	#		mac = r.target_addr
+	#		service = r.service
+	#		port = r.port
+	#		self.devices.append(Device(mac, service, port, self.source_id, self.verbose))
+	#	self.num_devices = len(self.devices)
+	#	return self.devices
 
 	def get_lights(self):
-		if self.num_devices == None:
-			responses = self.discover()
+		if self.lights == None:
+			self.lights = []
+			self.devices = []
+			if self.num_lights == None:
+				responses = self.discover()
+			else:
+				responses = self.broadcast_with_resp(GetService, StateService)
+			for r in responses:
+				mac = r.target_addr
+				service = r.service
+				port = r.port
+				light = Light(mac, service, port, self.source_id, self.verbose)
+				self.lights.append(light)
+				self.devices.append(light)
+			self.num_lights = len(self.lights)
+			self.num_devices = len(self.lights)
+		return self.lights
+
+	# returns dict of Light: power_level pairs
+	def get_power_all_lights(self):
+		responses = self.broadcast_with_resp(LightGetPower, LightStatePower)
+		power_states = []
+		for light in self.lights:
+			for response in responses:
+				if light.mac_addr == response.target_addr:
+					power_states.append((light, response.power_level))
+		return power_states
+
+	def set_power_all_lights(self, power_level, duration=0, rapid=False):
+		on = [True, 1, "on", 65535]
+		off = [False, 0, "off"]
+		try:
+			if power_level in on and not rapid:
+				self.broadcast_with_ack(LightSetPower, {"power_level": 65535, "duration": duration})
+			elif power_level in on and rapid:
+				self.broadcast_fire_and_forget(LightSetPower, {"power_level": 65535, "duration": duration}, num_repeats=5)
+			elif power_level in off and not rapid:
+				self.broadcast_with_ack(LightSetPower, {"power_level": 0, "duration": duration})
+			elif power_level in off and rapid:
+				self.broadcast_fire_and_forget(LightSetPower, {"power_level": 0, "duration": duration}, num_repeats=5)
+			else:
+				print("{} is not a valid power level.".format(power_level))
+		except WorkflowException as e:
+			print(e)
+
+	def get_color_all_lights(self):
+		responses = self.broadcast_with_resp(LightGet, LightState)
+		colors = []
+		for light in self.lights:
+			for response in responses:
+				if light.mac_addr == response.target_addr:
+					colors.append((light, response.color))
+		return colors
+
+	def set_color_all_lights(self, color, duration=0, rapid=False):
+		if len(color) == 4:
+			try:
+				if rapid:
+					self.broadcast_fire_and_forget(LightSetColor, {"color": color, "duration": duration}, num_repeats=5)
+				else:
+					self.broadcast_with_ack(LightSetColor, {"color": color, "duration": duration})
+			except WorkflowException as e:
+				print(e)
 		else:
-			responses = self.broadcast_with_resp(GetService, StateService)
-		for r in responses:
-			mac = r.target_addr
-			service = r.service
-			port = r.port
-			self.devices.append(Light(mac, service, port, self.source_id, self.verbose))
-		self.num_devices = len(self.devices)
-		return self.devices
-
-	# set_all_power(on/off)
-
-	# set_all_color(color, duration, rapid)
+			print("{} is not a valid power level.".format(power_level))
 
 	############################################################################
 	#                                                                          #
@@ -91,7 +146,7 @@ class LifxLAN:
 		self.close_socket()
 		return responses
 
-	def broadcast_fire_and_forget(self, msg_type, payload={}, timeout_secs=0.5, num_repeats=5):
+	def broadcast_fire_and_forget(self, msg_type, payload={}, timeout_secs=DEFAULT_TIMEOUT, num_repeats=DEFAULT_ATTEMPTS):
 		self.initialize_socket(timeout_secs)
 		msg = msg_type(BROADCAST_MAC, self.source_id, seq_num=0, payload=payload, ack_requested=False, response_requested=False)
 		sent_msg_count = 0
@@ -104,7 +159,7 @@ class LifxLAN:
 			sleep(sleep_interval) # Max num of messages device can handle is 20 per second.
 		self.close_socket()
 
-	def broadcast_with_resp(self, msg_type, response_type, payload={}, timeout_secs=3, max_attempts=5):
+	def broadcast_with_resp(self, msg_type, response_type, payload={}, timeout_secs=DEFAULT_TIMEOUT, max_attempts=DEFAULT_ATTEMPTS):
 		success = False
 		self.initialize_socket(timeout_secs)
 		if response_type == Acknowledgement:
@@ -147,11 +202,11 @@ class LifxLAN:
 		self.close_socket()
 		return responses
 
-	def broadcast_with_ack(self, msg_type, response_type, payload={}, timeout_sec=3, max_attempts=5):
-		broadcast_with_resp(msg_type, Acknowlegement, payload, timeout_secs, max_attempts)
+	def broadcast_with_ack(self, msg_type, payload={}, timeout_secs=DEFAULT_TIMEOUT+0.5, max_attempts=DEFAULT_ATTEMPTS):
+		self.broadcast_with_resp(msg_type, Acknowledgement, payload, timeout_secs, max_attempts)
 
 	# Not currently implemented, although the LIFX LAN protocol supports this kind of workflow natively
-	def broadcast_with_ack_resp(self, msg_type, response_type, payload={}, timeout_sec=3):
+	def broadcast_with_ack_resp(self, msg_type, response_type, payload={}, timeout_secs=DEFAULT_TIMEOUT+0.5, max_attempts=DEFAULT_ATTEMPTS):
 		pass
 
 	############################################################################
