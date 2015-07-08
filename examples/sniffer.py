a06f5e3ef76106929fecaa9f51a6f2dd4150cc84
mclarkk <meghan.leah.clark@gmail.com>
2015-07-08 09:22:46 -0400

    omg tabs

--- examples/sniffer.py
+++ examples/sniffer.py
@@ -8,40 +8,40 @@ from lifxlan import *
 from time import time
 
 class Sniffer(object):
-	def __init__(self):
-		self.port = UDP_BROADCAST_PORT
-		self.sock = None
-		self.sniff()
+    def __init__(self):
+        self.port = UDP_BROADCAST_PORT
+        self.sock = None
+        self.sniff()
 
-	def sniff(self):
-			self.initialize_socket()
-			try:
-				while(True):
-					try: 
-						data = self.sock.recv(1024)
-						request = unpack_lifx_message(data)
-						print("\nRECV:"),
-						print(request)
-					except timeout:
-						pass
-			except KeyboardInterrupt:
-				self.sock.close()
+    def sniff(self):
+            self.initialize_socket()
+            try:
+                while(True):
+                    try: 
+                        data = self.sock.recv(1024)
+                        request = unpack_lifx_message(data)
+                        print("\nRECV:"),
+                        print(request)
+                    except timeout:
+                        pass
+            except KeyboardInterrupt:
+                self.sock.close()
 
-	def send(self, msg):
-		if self.sock == None:
-			self.initialize_socket()
-		msg.origin = 1
-		print("SEND:"),
-		print(msg)
-		self.sock.sendto(msg.packed_message, (UDP_BROADCAST_IP, self.port))
+    def send(self, msg):
+        if self.sock == None:
+            self.initialize_socket()
+        msg.origin = 1
+        print("SEND:"),
+        print(msg)
+        self.sock.sendto(msg.packed_message, (UDP_BROADCAST_IP, self.port))
 
-	def initialize_socket(self):
-		self.sock = socket(AF_INET, SOCK_DGRAM)
-		self.sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-		self.sock.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
-		self.sock.settimeout(0.5)
-		port = UDP_BROADCAST_PORT
-		self.sock.bind(("", port))
+    def initialize_socket(self):
+        self.sock = socket(AF_INET, SOCK_DGRAM)
+        self.sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        self.sock.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+        self.sock.settimeout(0.5)
+        port = UDP_BROADCAST_PORT
+        self.sock.bind(("", port))
 
 if __name__ == "__main__":
-	Sniffer()
+    Sniffer()
