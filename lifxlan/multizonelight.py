e05b0b0bb287d2508e4c2e773cece3093a8f1e13
Meghan <meghan.leah.clark@gmail.com>
2016-12-21 02:50:07 -0500

    Made first steps towards making the get_color_zones function return the colors from all the zones on the device instead of the zone count, which doesn't make semantic sense (best thing would be to return a list of all the current zone colors, then if you want the count you get the length of the list). Turns out MultiZoneLights will need to have their own send/recv functions to deal with the fact that a device might send multiple individual StateZone messages, which sounds terrible unless you know beforehand how many to wait for. Also, get_color_zones will need to do extra logic and send multiple packets in order to get the state of all the zones on the device instead of just eight. Will open issue. Also, added function to set all the zone colors at once with a list of colors, so you can request colors from get_color_zones and then restore state after your light show by putting the output of that function back into set_zone_colors.

--- lifxlan/multizonelight.py
+++ lifxlan/multizonelight.py
@@ -6,29 +6,44 @@ from device import Device, WorkflowException
 from msgtypes import *
 
 class MultiZoneLight(Light):
-    def __init__(self,light, verbose=False):
-        super(MultiZoneLight, self).__init__(light.get_mac_addr(),light.get_ip_addr(),light.get_service(),light.get_port(), light.get_source_id(), verbose)
+    def __init__(self, mac_addr, ip_addr, service=1, port=56700, source_id=0, verbose=False):
+        super(MultiZoneLight, self).__init__(mac_addr, ip_addr)
 
-    def get_color_zones(self):
+
+    # This only gets the first StateMultiZone/StateZone response packet,
+    # but there may be several!!!
+    # Really need a special MultiZone send/recv function!
+    # right now can fake it by making multiple requests with different indices
+    def get_color_zones(self, start=0, end=255):
         try:
-            response = self.req_with_resp(LightGetColorZones, LightStateZone)
-            self.count = response.count
+            response = self.req_with_resp(MultiZoneGetColorZones, MultiZoneStateMultiZone, {"start_index":start, "end_index":end})
+            self.color = response.color
         except WorkflowException as e:
-            print(e)
-        return self.count
-
-        # color is [Hue, Saturation, Brightness, Kelvin], duration in ms
+            try:
+                response = self.req_with_resp(MultiZoneGetColorZones, MultiZoneStateZone, {"start_index":start, "end_index":end})
+                self.color = response.color
+            except WorkflowException as e:
+                print(e)
+        return self.color
 
     def set_zone_color(self, start_index, end_index, color, duration=0, rapid=False, apply=1):
         if len(color) == 4:
             try:
                 if rapid:
-                    self.fire_and_forget(LightSetColorZones,
+                    self.fire_and_forget(MultiZoneSetColorZones,
                                          {"start_index": start_index, "end_index": end_index, "color": color,
                                           "duration": duration, "apply": apply}, num_repeats=1)
                 else:
-                    self.req_with_ack(LightSetColorZones,
+                    self.req_with_ack(MultiZoneSetColorZones,
                                       {"start_index": start_index, "end_index": end_index, "color": color,
                                        "duration": duration, "apply": apply})
             except WorkflowException as e:
                 print(e)
+
+    # Sets colors for all zones given a list of HSVK colors
+    def set_zone_colors(self, colors, duration=0, rapid=False):
+        for (i, color) in enumerate(colors):
+            apply = 0
+            if i == len(colors)-1:
+                apply = 1
+            self.set_zone_color(i, i+1, color, duration, rapid, apply)
