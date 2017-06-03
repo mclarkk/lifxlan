4dfbbdec4b3233b246b190257f2c19a2d567fe30
Meghan <meghan.leah.clark@gmail.com>
2017-06-03 00:31:31 -0400

    updated version to reflect bug fix to make code compatible with latest Gen 3 firmware

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 #from setuptools import setup
 
 setup(name='lifxlan',
-      version='1.0.2',
+      version='1.0.3',
       description='API for local communication with LIFX devices over a LAN.',
       url='http://github.com/mclarkk/lifxlan',
       author='Meghan Clark',
