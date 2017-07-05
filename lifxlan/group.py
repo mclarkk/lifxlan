87d7cb15fff8b316b7ad2fac73dda2eb125115dd
Meghan <meghan.leah.clark@gmail.com>
2017-07-05 11:18:11 -0400

    first pass at a multithreaded generate Group class. Requires testing, then can add functions to the LifxLAN class to get groups by group name or by location. Also need to add functionality to LifxLAN to get device object by name.

---
+++ lifxlan/group.py
@@ -0,0 +1,71 @@
+import thread
+
+class Group(object):
+
+    def __init__(self, devices=[], verbose=False):
+        self.devices = devices
+        self.verbose = verbose
+
+    def add_device(self, device_object):
+        self.devices.append(device_object)
+
+    #def add_device_by_name(self, device_name):
+
+    def remove_device(self, device_object):
+        new_devices = []
+        for d in self.devices:
+            if d != device_object:
+                new_devices.append(d)
+        self.devices = new_devices
+
+    #def remove_device_by_name(self, device_name):
+
+    def set_power(self, power, duration=0, rapid=False):
+        for d in self.devices:
+            thread.start_new_thread(self.set_power_helper, (power, duration, rapid))
+
+    def set_power_helper(self, power, duration, rapid):
+        try:
+            d.set_power(power, duration, rapid) # Light::set_power(power, [duration], [rapid])
+        except TypeError:
+            d.set_power(power, rapid) # Device::set_power(power, [rapid])
+
+    def set_color(self, color, duration=0, rapid=False):
+        for d in self.devices:
+            if d.supports_color():
+                thread.start_new_thread(d.set_color, (color, duration, rapid))
+
+    def set_hue(self, hue, duration=0, rapid=False):
+        for d in self.devices:
+            if d.supports_color():
+                thread.start_new_thread(d.set_hue, (hue, duration, rapid))
+
+    def set_brightness(self, brightness, duration=0, rapid=False):
+        for d in self.devices:
+            if d.supports_color():
+                thread.start_new_thread(d.set_brightness, (brightness, duration, rapid))
+
+    def set_saturation(self, saturation, duration=0, rapid=False):
+        for d in self.devices:
+            if d.supports_color():
+                thread.start_new_thread(d.set_saturation, (saturation, duration, rapid))
+
+    def set_colortemp(self, kelvin, duration=0, rapid=False):
+        for d in self.devices:
+            if d.supports_color():
+                thread.start_new_thread(d.set_colortemp, (kelvin, duration, rapid))
+
+    def set_infrared(self, infrared_brightness):
+        for d in self.devices:
+            if d.supports_infrared():
+                thread.start_new_thread(d.set_infrared, (infrared_brightness))
+
+    def set_zone_color(self, start, end, color, duration=0, rapid=False, apply=1):
+        for d in self.devices:
+            if d.supports_multizone():
+                thread.start_new_thread(d.set_zone_color, (start, end, color, duration, rapid, apply))
+
+    def set_zone_colors(self, colors, duration=0, rapid=False):
+        for d in self.devices:
+            if d.supports_multizone():
+                thread.start_new_thread(d.set_zone_colors, (colors, duration, rapid, apply))
