55d3a2106cfc8ca8f9326accc6441d7f3ceae7e0
Meghan <meghan.leah.clark@gmail.com>
2017-07-07 18:13:23 -0400

    updated version number to reflect compatibility with upcoming firmware update (now they're checking message sizes, and the original calculation was wrong and has been fixed

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 #from setuptools import setup
 
 setup(name='lifxlan',
-      version='1.1.0',
+      version='1.1.1',
       description='API for local communication with LIFX devices over a LAN.',
       url='http://github.com/mclarkk/lifxlan',
       author='Meghan Clark',
