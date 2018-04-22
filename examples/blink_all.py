4842b7e31941fde599ad9e9688dcdcf466b79aa0
Meghan <meghan.leah.clark@gmail.com>
2018-04-22 15:25:27 -0700

    fixed a bug with rapid in the power control function. Only showed up when number of bulbs was not specified

--- examples/blink_all.py
+++ examples/blink_all.py
@@ -27,8 +27,6 @@ def main():
     print("Turning lights on...")
     lifx.set_power_all_lights("on")
 
-    sleep(0.2)
-
     print("Toggling power of all lights...")
     toggle_all_lights_power(lifx, 0.2)
 
@@ -36,41 +34,35 @@ def main():
     for light in original_powers:
         light.set_power(original_powers[light])
 
-
     # test color control
     original_colors = lifx.get_color_all_lights()
 
     print("Turning lights on...")
     lifx.set_power_all_lights("on")
 
-    sleep(1)
-
     print("Toggling color of all lights quickly...")
     toggle_all_lights_color(lifx, 0.2)
 
     print("Toggling color of all lights slowly...")
-    toggle_all_lights_color(lifx, 1)
+    toggle_all_lights_color(lifx, 0.5)
 
     print("Restoring original color to all lights...")
     for light in original_colors:
         light.set_color(original_colors[light])
 
-    sleep(1)
+    sleep(0.2)
 
     print("Restoring original power to all lights...")
     for light in original_powers:
         light.set_power(original_powers[light])
 
 
-
-
 def toggle_all_lights_power(lan, interval=0.5, num_cycles=3): #TEST
-    lan.set_power_all_lights("off")
     rapid = True if interval < 1 else False
     for i in range(num_cycles):
-        lan.set_power_all_lights("on", rapid)
+        lan.set_power_all_lights("on", rapid=rapid)
         sleep(interval)
-        lan.set_power_all_lights("off", rapid)
+        lan.set_power_all_lights("off", rapid=rapid)
         sleep(interval)
 
 def toggle_all_lights_color(lan, interval=0.5, num_cycles=3):
