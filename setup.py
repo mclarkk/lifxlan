50516738036ad31799ef66e3cf8e540abfb23a51
Meghan <meghan.leah.clark@gmail.com>
2021-03-19 17:51:02 -0700

    switched to setuptools

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
-from distutils.core import setup
-#from setuptools import setup
+from setuptools import setup
 import re
 
 with open("lifxlan/__init__.py") as meta_file:
@@ -25,12 +24,5 @@ setup(name='lifxlan',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6'
+        'Programming Language :: Python :: 3'
       ])
